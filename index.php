f90239312b49d3333420c4f0dd36f56e5369a2a6
VarshaS-eng <57896894+VarshaS-eng@users.noreply.github.com>
2020-01-13 12:09:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,25 +8,19 @@ if (empty($_POST['fname']) && empty($_POST['email'])) die();
 if ($_POST)
 	{
 
-	// set response code - 200 OK
-
 	http_response_code(200);
 	$subject = $_POST['fname'];
 	$to = "me@malith.pro";
 	$from = $_POST['email'];
 
-	// data
-
 	$msg = $_POST['number'] . $_POST['message'];
 
-	// Headers
 
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers.= "Content-type: text/html; charset=UTF-8\r\n";
 	$headers.= "From: <" . $from . ">";
 	mail($to, $subject, $msg, $headers);
 
-	// echo json_encode( $_POST );
 
 	echojson_encode(array(
 		"sent" => true
@@ -34,8 +28,6 @@ if ($_POST)
 	}
   else
 	{
-
-	// tell the user about error
 
 	echojson_encode(["sent" => false, "message" => "Something went wrong"]);
 	}
